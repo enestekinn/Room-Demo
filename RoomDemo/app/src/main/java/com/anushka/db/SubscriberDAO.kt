fb4d7d213d7230bda0857c905e69027746dbb079
Enes Tekin <>
2020-12-28 16:25:02 +0300

    DAO Interface

---
+++ RoomDemo/app/src/main/java/com/anushka/db/SubscriberDAO.kt
@@ -0,0 +1,44 @@
+package com.anushka.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun  insertSubscriber(subscriber: Subscriber)
+
+    @Update
+    suspend fun  updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun  deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers() : LiveData<List<Subscriber>>
+
+
+
+
+
+    //Variation of insert function
+    /*
+
+    @Insert
+    suspend fun  insertSubscriber2(subscriber: Subscriber) : Long
+
+    @Insert
+    suspend fun  insertSubscribers(subscriber1: Subscriber,subscriber2: Subscriber,subscriber3: Subscriber) : List<Long>
+
+    @Insert
+    fun insertSubscribers2(subscriber : Subscriber , subscribers : List<Subscriber>) : List<Long>
+*/
+
+
+}
