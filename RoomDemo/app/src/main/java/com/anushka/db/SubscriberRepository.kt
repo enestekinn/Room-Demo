176d4f6c5607f7ff44871924ee008696b062f212
Enes Tekin <>
2020-12-28 22:44:11 +0300

    Activity Main Layout

--- RoomDemo/app/src/main/java/com/anushka/db/SubscriberRepository.kt
+++ RoomDemo/app/src/main/java/com/anushka/db/SubscriberRepository.kt
@@ -5,7 +5,7 @@ class SubscriberRepository (private val dao : SubscriberDAO) {
     val subscriberDAO = dao.getAllSubscribers()
 
     suspend fun insert(subscriber: Subscriber) {
-        dao.insertSubscriber(subscriber#)
+        dao.insertSubscriber(subscriber)
     }
 
     suspend fun update(subscriber: Subscriber) {
